4c36138040a406300f3e1dfcb2fedbf1823ca722
金正朋也 <mhtnim1023@gmail.com>
2019-10-08 02:05:45 +0900

    テスト追記

--- counter.js
+++ counter.js
@@ -5,12 +5,20 @@ export default {
     <div>
       <span class="count">{{ count }}</span>
       <button @click="increment">Increment</button>
+      <p class="moreThan8TotalHits" v-if="totalHits >= 8">データは8件以上です</p>
+      <p class="lessThan8TotalHits" v-else>データは8件未満です</p>
+      <p class="job-companyName">{{ job.companyName }}</p>
     </div>
   `,
 
   data () {
     return {
-      count: 0
+      count: 0,
+      totalHits: 2,
+      job: {
+        companyName: "",
+        siteName: "an"
+      }
     }
   },
 
--- test.js
+++ test.js
@@ -5,7 +5,10 @@ import Counter from './counter'
 
 describe('Counter', () => {
   // Now mount the component and you have the wrapper
+  // コンポーネントをラップする
   const wrapper = mount(Counter)
+  // vmを介することで実際のコンポーネントにアクセスできる
+  // const vm = wrapper.vm
 
   it('renders the correct markup', () => {
     expect(wrapper.html()).toContain('<span class="count">0</span>')
@@ -21,5 +24,27 @@ describe('Counter', () => {
     const button = wrapper.find('button')
     button.trigger('click')
     expect(wrapper.vm.count).toBe(1)
+  })
+
+  it('totalData is more than 8', () => {
+    // dataを定義することができる
+    wrapper.setData({totalHits: 10})
+    expect(wrapper.contains(".moreThan8TotalHits")).toBe(true)
+  })
+
+  it('totalData is less than 8', () => {
+    wrapper.setData({totalHits: 5})
+    expect(wrapper.contains(".moreThan8TotalHits")).toBe(false)
+    expect(wrapper.contains(".lessThan8TotalHits")).toBe(true)
+  })
+
+  it('display nothing when company name is empty', () => {
+    wrapper.setData({job: {companyName: ""}})
+    expect(wrapper.html()).toContain('<p class="job-companyName"></p>')
+  })
+
+  it('display nothing when company name is empty', () => {
+    wrapper.setData({job: {companyName: "bizreach"}})
+    expect(wrapper.html()).toContain('<p class="job-companyName">bizreach</p>')
   })
 })
